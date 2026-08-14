d586c1303f5cd70fd05be9499f27bad639321240
Nica-17 <vj.nica17@gmail.com>
2026-08-13 18:23:34 -0700

    Configure MySQL database connection

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,5 +1,15 @@
+require('dotenv').config();
+
 const config = {
     port: process.env.PORT || 3000,
+
+    db: {
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 3306,
+        name: process.env.DB_NAME || 'mosalinx_dev',
+        user: process.env.DB_USER || 'mosalinx_dev',
+        password: process.env.DB_PASSWORD,
+    },
 };
 
 module.exports = config;
