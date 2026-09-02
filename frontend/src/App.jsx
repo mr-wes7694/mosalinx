f99d0084b628fcf814b805d0d15befdc5160f882
Wesley Filion <mr.wes7694@gmail.com>
2026-09-02 18:33:18 -0400

    Preserving protected route, switch "Dashboard" for "Shell" in app.jsx. Dashboard will become a placeholder for the bulletin board app.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,24 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
-import Signup from './pages/Signup.jsx'
 import Shell from './components/Shell.jsx'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
+import Signup from './pages/Signup.jsx'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Shell />
+          </ProtectedRoute>
+        }
+      />
       <Route path="/signup" element={<Signup />} />
-
-      <Route path="/app" element={<Shell />}>
-        <Route index element={<h1>Project Workspaces</h1>} />
-      </Route>
     </Routes>
   )
 }
