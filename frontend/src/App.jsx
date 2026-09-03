954be3676c9b4bb80a5ae006e01cbb97db62dd5c
Wesley Filion <mr.wes7694@gmail.com>
2026-09-02 21:53:15 -0400

    Update App.jsx
    Correct import for Shell

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
-import Dashboard from './pages/Dashboard.jsx'
+import Shell from './components/Shell.jsx'
 import ProtectedRoute from './components/ProtectedRoute.jsx'
 
 function App() {
